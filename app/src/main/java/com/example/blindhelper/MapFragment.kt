64fefc000017e425e149571f2279cbb8f160d257
L0CUST <namae01@naver.com>
2023-05-07 01:44:33 +0900

    지도 프래그먼트 개선

--- app/src/main/java/com/example/blindhelper/MapFragment.kt
+++ app/src/main/java/com/example/blindhelper/MapFragment.kt
@@ -1,19 +1,25 @@
 package com.example.blindhelper
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.core.content.PermissionChecker.checkSelfPermission
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.blindhelper.databinding.FragmentMapBinding
 import com.example.blindhelper.viewmodel.ObstacleViewModel
@@ -27,9 +33,17 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 @Suppress("DEPRECATION")
 class MapFragment : Fragment(), OnMapReadyCallback {
@@ -42,8 +56,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val callback = OnMapReadyCallback { googleMap ->
         mMap = googleMap
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity as MainActivity)
-        updateLocation()
     }
 
     override fun onCreateView(
@@ -131,14 +143,51 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val latlng = LatLng(lastLocation.latitude, lastLocation.longitude)
         val markerOptions = MarkerOptions()
             .position(latlng)
-            .title("Here!")
-
+            .title("현위치")
         val cameraPosition = CameraPosition.Builder()
             .target(latlng)
             .zoom(20.0f)
             .build()
         mMap.clear()
         mMap.addMarker(markerOptions)
+        mMap.addCircle(CircleOptions()
+            .center(latlng)
+            .radius(5.0)
+            .strokeColor(Color.RED)
+            .fillColor(Color.TRANSPARENT)
+        )
+
+        val data = obstacle.obstacles.value
+        if (data != null) {
+            for (dat in data) {
+                val obstaclePos = LatLng(dat.latitude, dat.longitude)
+                val mMarkerOptions = MarkerOptions()
+                    .position(obstaclePos)
+                    .title("장애물")
+                    .icon(bitmapDescriptorFromVector(activity as MainActivity, R.drawable.obstacle))
+                mMap.addMarker(mMarkerOptions)
+
+                val locA: Location = Location("")
+                locA.latitude = lastLocation.latitude
+                locA.longitude = lastLocation.longitude
+                val locB: Location = Location("")
+                locB.latitude = dat.latitude
+                locB.longitude = dat.longitude
+                val distance = locA.distanceTo(locB)
+                if (distance <= 5) {
+                    Toast.makeText(activity as MainActivity, "장애물 감지", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
     }
 }
